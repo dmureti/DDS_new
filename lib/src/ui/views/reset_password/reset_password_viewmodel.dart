b95bf2f902f6a854ae7d14e92091cd1e31deccbb
Albert Kamau <kamau.albert@gmail.com>
2021-04-21 01:15:57 +0300

    Added functionality to reset password

--- lib/src/ui/views/reset_password/reset_password_viewmodel.dart
+++ lib/src/ui/views/reset_password/reset_password_viewmodel.dart
@@ -1,3 +1,27 @@
+import 'package:distributor/app/locator.dart';
+import 'package:distributor/app/router.gr.dart';
+import 'package:distributor/services/user_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:tripletriocore/tripletriocore.dart';
 
-class ResetPasswordViewmodel extends BaseViewModel {}
+class ResetPasswordViewmodel extends BaseViewModel {
+  NavigationService _navigationService = locator<NavigationService>();
+  DialogService _dialogService = locator<DialogService>();
+  UserService _userService = locator<UserService>();
+
+  void resetPassword() async {
+    setBusy(true);
+    var result = await _userService.resetPassword();
+    setBusy(false);
+    if (result is CustomException) {
+      await _dialogService.showDialog(
+          title: result.title, description: result.description);
+    } else {
+      await _dialogService.showDialog(
+          title: 'Success',
+          description: 'Your password was reset successfully');
+      _navigationService.pushNamedAndRemoveUntil(Routes.loginView);
+    }
+  }
+}
