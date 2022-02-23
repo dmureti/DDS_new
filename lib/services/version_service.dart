f38ad08770a7913037e3af144b8417588ac61bd5
Albert Kamau <kamau.albert@gmail.com>
2022-02-23 06:15:23 +0300

    version update

--- lib/services/version_service.dart
+++ lib/services/version_service.dart
@@ -21,7 +21,7 @@ class VersionService {
   // String get version => packageInfo.version;
   // String get buildNumber => packageInfo.buildNumber;
 
-  String get version => '8.28.3';
+  String get version => '8.28.6';
 
   getVersion() async {
     // _packageInfo = await PackageInfo.fromPlatform();
