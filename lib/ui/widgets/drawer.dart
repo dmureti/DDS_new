4b81c76c251661ed24cec1aeb544f6f9ae2b4ad9
Albert Kamau <kamau.albert@gmail.com>
2022-09-11 21:24:24 +0300

    Added sidebar help

--- lib/ui/widgets/drawer.dart
+++ lib/ui/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:distributor/conf/style/lib/colors.dart';
 import 'package:distributor/ui/views/home/home_viewmodel.dart';
+import 'package:distributor/ui/widgets/drawer_button/drawer_secondary_button.dart';
 import 'package:distributor/ui/widgets/dumb_widgets/drawer_listTile.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked_hooks/stacked_hooks.dart';
@@ -18,118 +19,145 @@ class CustomDrawer extends HookViewModelWidget<HomeViewModel> {
         //   alignment: Alignment.bottomCenter,
         // ),
       ),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-              child: Row(
-            children: <Widget>[
-              Expanded(
-                child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                  ),
-                  accountEmail: Text(
-                    '${model.user.email ?? model.user.mobile}',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  accountName: Text(
-                    '${model.user.full_name}',
-                    style: TextStyle(color: Colors.black),
-                  ),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(color: kColorDDSPrimaryDark),
+            width: MediaQuery.of(context).size.width * .15,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                DrawerSecondaryButton(
+                  iconData: Icons.help,
+                  onTap: model.navigateToHelpView,
                 ),
-              ),
-            ],
-          )),
-          DrawerListTile(
-            isEnabled: model.enableHomeTab,
-            label: 'Home',
-            onTap: () {
-              Navigator.pop(context);
-              model.navigateToHome(0);
-            },
-            iconData: Icons.home,
-          ),
-          DrawerListTile(
-            label: 'Journey',
-            onTap: () {
-              Navigator.pop(context);
-              model.navigateToHome(1);
-            },
-            iconData: Icons.swap_calls,
-            isEnabled: model.enableJourneyTab,
-          ),
-          DrawerListTile(
-            isEnabled: model.enableAdhocTab,
-            label: 'Adhoc Sales',
-            onTap: () {
-              Navigator.pop(context);
-              model.navigateToHome(2);
-            },
-            iconData: Icons.add_shopping_cart,
-          ),
-          DrawerListTile(
-            isEnabled: model.enableProductTab,
-            label: 'Stock Controller',
-            onTap: () {
-              Navigator.pop(context);
-              model.navigateToHome(3);
-            },
-            iconData: Icons.apps,
-          ),
-
-          DrawerListTile(
-            isEnabled: model.enableCustomerTab,
-            label: 'Customers',
-            onTap: () {
-              Navigator.pop(context);
-              model.navigateToHome(4);
-            },
-            iconData: Icons.people,
-          ),
-          // ListTile(
-          //   trailing: Icon(Icons.notifications),
-          //   title: Text('Notifications'),
-          //   onTap: () {
-          //     Navigator.pop(context);
-          //     model.navigateToHome(5);
-          //   },
-          // ),
-          Divider(),
-
-          ListTile(
-            title: Text(
-              'CHANGE PASSWORD',
-              style: TextStyle(
-                  fontFamily: 'NerisBlack', color: kColDDSPrimaryDark),
-            ),
-            onTap: () async {
-              Navigator.pop(context);
-              model.navigateToChangePassword();
-            },
-          ),
-
-          ListTile(
-            title: Text(
-              'LOG OUT',
-              style: TextStyle(
-                  fontFamily: 'NerisBlack', color: kColDDSPrimaryDark),
-            ),
-            onTap: () async {
-              model.logout();
-            },
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(model.appEnv.name),
+                DrawerSecondaryButton(
+                  iconData: Icons.info,
+                  onTap: model.navigateToInfoView,
+                ),
+                DrawerSecondaryButton(
+                  iconData: Icons.bug_report_sharp,
+                  onTap: model.navigateToBugView,
+                ),
+              ],
             ),
           ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                    child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: UserAccountsDrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                        ),
+                        accountEmail: Text(
+                          '${model.user.email ?? model.user.mobile}',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        accountName: Text(
+                          '${model.user.full_name}',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+                DrawerListTile(
+                  isEnabled: model.enableHomeTab,
+                  label: 'Home',
+                  onTap: () {
+                    Navigator.pop(context);
+                    model.navigateToHome(0);
+                  },
+                  iconData: Icons.home,
+                ),
+                DrawerListTile(
+                  label: 'Journey',
+                  onTap: () {
+                    Navigator.pop(context);
+                    model.navigateToHome(1);
+                  },
+                  iconData: Icons.swap_calls,
+                  isEnabled: model.enableJourneyTab,
+                ),
+                DrawerListTile(
+                  isEnabled: model.enableAdhocTab,
+                  label: 'Adhoc Sales',
+                  onTap: () {
+                    Navigator.pop(context);
+                    model.navigateToHome(2);
+                  },
+                  iconData: Icons.add_shopping_cart,
+                ),
+                DrawerListTile(
+                  isEnabled: model.enableProductTab,
+                  label: 'Stock Controller',
+                  onTap: () {
+                    Navigator.pop(context);
+                    model.navigateToHome(3);
+                  },
+                  iconData: Icons.apps,
+                ),
 
-          Center(
-            child: Padding(
-              child: Text('Version : ${model.version}'),
-              padding: const EdgeInsets.all(8.0),
+                DrawerListTile(
+                  isEnabled: model.enableCustomerTab,
+                  label: 'Customers',
+                  onTap: () {
+                    Navigator.pop(context);
+                    model.navigateToHome(4);
+                  },
+                  iconData: Icons.people,
+                ),
+                // ListTile(
+                //   trailing: Icon(Icons.notifications),
+                //   title: Text('Notifications'),
+                //   onTap: () {
+                //     Navigator.pop(context);
+                //     model.navigateToHome(5);
+                //   },
+                // ),
+                Divider(),
+
+                ListTile(
+                  title: Text(
+                    'CHANGE PASSWORD',
+                    style: TextStyle(
+                        fontFamily: 'NerisBlack', color: kColDDSPrimaryDark),
+                  ),
+                  onTap: () async {
+                    Navigator.pop(context);
+                    model.navigateToChangePassword();
+                  },
+                ),
+
+                ListTile(
+                  title: Text(
+                    'LOG OUT',
+                    style: TextStyle(
+                        fontFamily: 'NerisBlack', color: kColDDSPrimaryDark),
+                  ),
+                  onTap: () async {
+                    model.logout();
+                  },
+                ),
+                // Center(
+                //   child: Padding(
+                //     padding: const EdgeInsets.all(8.0),
+                //     child: Text(model.appEnv.name),
+                //   ),
+                // ),
+                //
+                // Center(
+                //   child: Padding(
+                //     child: Text('Version : ${model.version}'),
+                //     padding: const EdgeInsets.all(8.0),
+                //   ),
+                // ),
+              ],
             ),
           ),
         ],
