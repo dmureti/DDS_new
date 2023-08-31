7e500f9fb813d76b26c4953b60c9c934c70fde80
Albert Kamau <kamau.albert@gmail.com>
2023-09-01 00:50:58 +0300

    Updated to handle errors thrown

--- lib/src/ui/views/voucher_detail/voucher_detail_viewmodel.dart
+++ lib/src/ui/views/voucher_detail/voucher_detail_viewmodel.dart
@@ -2,7 +2,6 @@ import 'package:distributor/app/locator.dart';
 import 'package:distributor/core/models/app_models.dart';
 import 'package:distributor/services/user_service.dart';
 import 'package:distributor/src/ui/common/transaction_viewmodel.dart';
-
 import 'package:stacked_services/stacked_services.dart';
 
 class VoucherDetailViewmodel extends TransactionViewmodel {
@@ -69,18 +68,21 @@ class VoucherDetailViewmodel extends TransactionViewmodel {
         cancelTitle: 'NO',
         confirmationTitle: 'Yes');
     if (dialogResponse.confirmed) {
-      await commitStatusChange(val);
-      _disableDropdown = true;
-      await _dialogService.showDialog(
-          title: 'Success',
-          description: 'The transaction has been ${val}ed successfully');
-      getTransaction();
-      _navigationService.back(result: true);
-    } else {
-      await _dialogService.showDialog(
-          title: 'Status Not Changed',
-          description: 'The status of the transaction has not changed.');
-      _status = null;
+      var response = await commitStatusChange(val);
+      if (response is bool) {
+        await _dialogService.showDialog(
+            title: 'Success',
+            description: 'The transaction has been ${val}ed successfully');
+        _disableDropdown = true;
+        getTransaction();
+        _navigationService.back(result: true);
+      } else {
+        await _dialogService.showDialog(
+            title: 'Pending Transaction Error',
+            description: response.toString());
+        _status = null;
+        _navigationService.back(result: true);
+      }
     }
     notifyListeners();
   }
@@ -91,15 +93,12 @@ class VoucherDetailViewmodel extends TransactionViewmodel {
   commitStatusChange(String val) async {
     switch (status.toLowerCase()) {
       case 'accept':
-        approveTransaction(stockTransaction);
-        return;
+        return await approveTransaction(stockTransaction);
         break;
       case 'cancel':
-        cancelTransaction(stockTransaction);
-        return;
+        return await cancelTransaction(stockTransaction);
         break;
     }
-    return;
   }
 
   showConfirmationDialog() async {
