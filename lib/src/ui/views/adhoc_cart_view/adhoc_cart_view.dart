95d4b20faa36d9310e891aeec49651f6fc2effde
Albert Kamau <kamau.albert@gmail.com>
2024-06-20 19:17:16 +0300

    Removed dismissable - Fix issue with va;lidation of SKU quantities on swipe

--- lib/src/ui/views/adhoc_cart_view/adhoc_cart_view.dart
+++ lib/src/ui/views/adhoc_cart_view/adhoc_cart_view.dart
@@ -58,145 +58,65 @@ class AdhocCartView extends StatelessWidget {
                             itemCount: model.productList.length,
                             itemBuilder: (context, index) {
                               var item = model.productList[index];
-                              return Dismissible(
-                                background: Container(
-                                  color: Colors.green,
-                                  child: Icon(
-                                    Icons.add_circle,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                secondaryBackground: Container(
-                                  color: Colors.red,
-                                  child: Icon(
-                                    Icons.remove_circle,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                confirmDismiss: (direction) async {
-                                  if (direction ==
-                                      DismissDirection.startToEnd) {
-                                    //Add to the quantity
-                                    var newVal = item.quantity + 1;
-                                    model.increaseSalesOrderItems(item, 1);
-                                    model.addToTotal(item.itemPrice);
-                                    // model.addItemQuantity();
+                              return ListTile(
+                                onTap: () async {
+                                  var result = await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return QuantityInput(
+                                        title: 'Enter Quantity',
+                                        minQuantity: 0,
+                                        maxQuantity:
+                                            item.initialQuantity.toInt(),
+                                        description:
+                                            'How many pcs for ${item.itemName} would you like to order ?',
+                                        initialQuantity:
+                                            model.getQuantity(item),
+                                      );
+                                    },
+                                  );
+                                  if (result != null) {
+                                    model.editQuantityManually(item, result);
+                                    model.addToTotal(result * item.itemPrice,
+                                        item: item);
                                     model.updateQuantity(
-                                        product: item, newVal: newVal);
-                                    // model.addToTotal(newVal * item.itemPrice,
-                                    //     item: item);
+                                        product: item, newVal: result);
                                   }
-                                  if (direction ==
-                                      DismissDirection.endToStart) {
-                                    //Reduce the quantity
-                                    var newVal = item.quantity - 1;
-                                    if (newVal >= 0) {
-                                      model.decreaseSalesOrderItems(item, 1);
-                                      model.removeFromTotal(item.itemPrice,
-                                          item: item);
-                                      // model.removeItemQuantity();
-                                      // model.decreaseSalesOrderItems(item, newVal);
-                                      model.updateQuantity(
-                                          product: item, newVal: newVal);
-                                      model.removeFromTotal(
-                                          newVal * item.itemPrice,
-                                          item: item);
-                                    }
-                                  }
-                                  return false;
                                 },
-                                key: Key(item.itemCode),
-                                child: ListTile(
-                                  onTap: () async {
-                                    var result = await showDialog(
-                                      context: context,
-                                      builder: (context) {
-                                        return QuantityInput(
-                                          title: 'Enter Quantity',
-                                          minQuantity: 0,
-                                          maxQuantity: item.initialQuantity.toInt(),
-                                          description:
-                                              'How many pcs for ${item.itemName} would you like to order ?',
-                                          initialQuantity:
-                                              model.getQuantity(item),
-                                        );
-                                      },
-                                    );
-                                    if (result != null) {
-                                      model.editQuantityManually(item, result);
-                                      model.addToTotal(result * item.itemPrice,
-                                          item: item);
-                                      model.updateQuantity(
-                                          product: item, newVal: result);
-                                    }
-                                    // var difference = await showQuantityDialog(
-                                    //     quantity: item.quantity, model: null);
-                                    // if (difference is int) {
-                                    //   num totalDifference =
-                                    //       difference * item.itemPrice;
-                                    //   //update the salesOrderViewmodel
-                                    //   model.addToTotal(totalDifference,
-                                    //       item: item);
-                                    //   // Get the difference in terms of quantity
-                                    //   num differenceInQuantity =
-                                    //       totalDifference / item.itemPrice;
-                                    //   if (differenceInQuantity != 0) {
-                                    //     /// Check if there was an overall reduction in cart items
-                                    //     /// If the difference is less than zero
-                                    //     /// The number of cart items shall reduce
-                                    //     if (differenceInQuantity < 0) {
-                                    //       model.decreaseSalesOrderItems(item,
-                                    //           (-(differenceInQuantity)).toInt());
-                                    //     } else {
-                                    //       model.increaseSalesOrderItems(
-                                    //           item, differenceInQuantity.toInt());
-                                    //     }
-                                    //   }
-                                    // }
-                                  },
-                                  // leading: Container(
-                                  //   width: 30,
-                                  //   height: 30,
-                                  //   decoration: BoxDecoration(
-                                  //       border: Border.all(width: 0.2)),
-                                  // ),
-                                  title: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        '${item.itemName}',
-                                        style: kTileLeadingTextStyle,
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${item.itemName}',
+                                      style: kTileLeadingTextStyle,
 //                    overflow: TextOverflow.ellipsis,
-                                      ),
-                                      SizedBox(
-                                        height: 2,
-                                      ),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            '${item.itemCode}',
-                                            style: kTileSubtitleTextStyle,
+                                    ),
+                                    SizedBox(
+                                      height: 2,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          '${item.itemCode}',
+                                          style: kTileSubtitleTextStyle,
 //                    overflow: TextOverflow.ellipsis,
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                  subtitle: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                          '${item.itemPrice.toStringAsFixed(2)}'),
-                                      Text(model
-                                          .getTotal(item)
-                                          .toStringAsFixed(2))
-                                    ],
-                                  ),
-                                  trailing:
-                                      Text(model.getQuantity(item).toString()),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
+                                subtitle: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                        '${item.itemPrice.toStringAsFixed(2)}'),
+                                    Text(
+                                        model.getTotal(item).toStringAsFixed(2))
+                                  ],
+                                ),
+                                trailing:
+                                    Text(model.getQuantity(item).toString()),
                               );
                             },
                           ),
