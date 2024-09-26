f41a592637fcc52bd1855277c6ad59968124aca8
Albert Kamau <kamau.albert@gmail.com>
2024-09-26 12:32:38 +0300

    Added functionality for pushing to SAP Added functionality to handle filters for pending and failed invoices Fixed issue with refreshing of filters for pending and failed invoices

--- lib/src/ui/views/pos/invoicing/invoicing_viewmodel.dart
+++ lib/src/ui/views/pos/invoicing/invoicing_viewmodel.dart
@@ -3,12 +3,14 @@ import 'package:distributor/app/router.gr.dart';
 import 'package:distributor/core/models/app_models.dart';
 import 'package:distributor/core/models/invoice.dart';
 import 'package:distributor/services/adhoc_cart_service.dart';
+import 'package:distributor/services/api_service.dart';
 import 'package:distributor/services/stock_controller_service.dart';
 import 'package:distributor/services/user_service.dart';
 import 'package:distributor/src/ui/views/pos/sales_returns/sales_returns_view.dart';
 import 'package:distributor/src/ui/views/print_view/print_view.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:tripletriocore/tripletriocore.dart';
 
 enum InvoiceType { pending, failed, finalized }
 
@@ -17,6 +19,15 @@ class InvoicingViewModel extends BaseViewModel {
   final _stockControllerService = locator<StockControllerService>();
   final _userService = locator<UserService>();
   final _adhocService = locator<AdhocCartService>();
+  final _apiService = locator<ApiService>();
+  final _dialogService = locator<DialogService>();
+
+  Api get api => _apiService.api;
+  User get user => _userService.user;
+  Invoice _finalizedInvoice;
+  Invoice get finalizedInvoice => _finalizedInvoice;
+
+  String get token => user.token;
 
   DateTime _startDate = DateTime.now();
   DateTime _endDate = DateTime.now();
@@ -80,7 +91,7 @@ class InvoicingViewModel extends BaseViewModel {
     setBusy(true);
     // _failedInvoices = await _stockControllerService.getInvoices("failed");
     var result = await _stockControllerService.getInvoices("failed");
-    if(result is List){
+    if (result is List) {
       _failedInvoices = result;
     }
     setBusy(false);
@@ -104,10 +115,61 @@ class InvoicingViewModel extends BaseViewModel {
     );
   }
 
+  finalizeOrder(var invoice) async {
+    var result = await api.finalizeSale(token: token, invoiceId: invoice['id']);
+    if (result is APIResponse) {
+      await _dialogService.showDialog(
+          title: result.error ?? "An error occurred",
+          description: result.errorMessage ?? "Unknown error");
+    } else {
+      AdhocDetail _adhocDetail = AdhocDetail(
+          saleItems: result['items'] ?? [],
+          transactionStatus: result['deliveryStatus'] ?? '',
+          total: result['total'],
+          verificationCode: result['verificationCode'] ?? "",
+          qrCode: result['qrCode'] ?? "",
+          deviceNo: result['deviceNo'] ?? "",
+          mode: "Online",
+          remarks: "",
+          warehouseId: result['deliveryWarehouse'] ?? "",
+          customerName: result['customerName'] ?? "",
+          deliveryType: result['deliveryType'] ?? "",
+          customerId: result['customerCode'] ?? "",
+          net: result['net'],
+          tax: result['tax'],
+          discount: result['discount'] ?? 0.00,
+          withholdingTax: result['withholding'] ?? 0.00,
+          sellingPriceList: result['sellingPriceList'] ?? "",
+          gross: result['gross'],
+          fdn: result['fdn'] ?? "");
+      // _customerTIN = result['customerTIN'];
+      _finalizedInvoice = Invoice.fromAdhocDetail(_adhocDetail, "Kshs");
+      notifyListeners();
+    }
+  }
+
   void updateFinalizedOrderRange(DateTime result) async {
     _startDate = result;
     setBusy(true);
     _finalizedInvoices = await _stockControllerService.getInvoices("finalized",
+        deliveryDate: result, endDate: endDate);
+    setBusy(false);
+    notifyListeners();
+  }
+
+  void updatePendingOrderRange(DateTime result) async {
+    _startDate = result;
+    setBusy(true);
+    _pendingInvoices = await _stockControllerService.getInvoices("pending",
+        deliveryDate: result, endDate: endDate);
+    setBusy(false);
+    notifyListeners();
+  }
+
+  void updateFailedOrderRange(DateTime result) async {
+    _startDate = result;
+    setBusy(true);
+    _failedInvoices = await _stockControllerService.getInvoices("failed",
         deliveryDate: result, endDate: endDate);
     setBusy(false);
     notifyListeners();
@@ -119,6 +181,11 @@ class InvoicingViewModel extends BaseViewModel {
     setBusy(true);
     _finalizedInvoices = await _stockControllerService.getInvoices("finalized",
         deliveryDate: startDate, endDate: endDate);
+    _failedInvoices = await _stockControllerService.getInvoices("failed",
+        deliveryDate: startDate, endDate: endDate);
+    _pendingInvoices = await _stockControllerService.getInvoices("pending",
+        deliveryDate: startDate, endDate: endDate);
+    // _pendingInvoices = await _stockControllerService.getInvoices("pending",deliveryDate: startDate, endDate: endDate);
     setBusy(false);
     notifyListeners();
   }
